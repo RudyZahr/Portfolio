fdcacc492d38313b29275f4bdbc70012ba4ebbc2
Rudy <49363226+RudyZahr@users.noreply.github.com>
2021-04-08 14:04:18 +0300

    fixed js bug

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,20 +1,20 @@
 function changeTheme() {
     if (document.body.style.color==="seashell"){
         document.body.style.color= "black";
-        document.body.querySelector(".link").style.color="black";
         document.body.style.backgroundColor="seashell";
         document.body.querySelector(".li1").style.color="black";
         document.body.querySelector(".li2").style.color="black";
         document.body.querySelector(".li3").style.color="black";
         document.body.querySelector(".li4").style.color="black";
+        document.body.querySelector(".link").style.color="black";
     } else {
         document.body.style.color = "seashell";
         document.body.style.backgroundColor="#171717";
-        document.body.querySelector(".link").style.color="seashell";
         document.body.querySelector(".li1").style.color="seashell";
         document.body.querySelector(".li2").style.color="seashell";
         document.body.querySelector(".li3").style.color="seashell";
         document.body.querySelector(".li4").style.color="seashell";
+        document.body.querySelector(".link").style.color="seashell";
     }
     
 }
